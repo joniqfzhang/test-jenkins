8373dacd919a8bea9400c1f47445e02fa9f1f474
Joni <qfzhang18@hotmail.com>
2023-09-12 12:05:32 +1000

    add spring mvn code

---
+++ src/main/java/com/example/testjenkins/TestJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.example.testjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SpringApplication.run(TestJenkinsApplication.class, args);
+	}
+
+}
