bff89af96d214710d1382dd6ab7fe80adceab391
Joni <qfzhang18@hotmail.com>
2023-09-12 12:29:13 +1000

    remove hello line

--- src/main/java/com/example/testjenkins/TestJenkinsApplication.java
+++ src/main/java/com/example/testjenkins/TestJenkinsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestJenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		SpringApplication.run(TestJenkinsApplication.class, args);
 	}
 
